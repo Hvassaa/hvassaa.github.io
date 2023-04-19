4eedbdacd93ab12e8ed571d6586fb057a65c8e8b
Rasmus T. Bjerg <rs.bjerg@.com>
2023-04-19 17:46:37 +0200

    new docs (table. etc)

---
+++ implementors/core/marker/trait.Unpin.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hacspec_halo2":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"hacspec_halo2/struct.PublicParams.html\" title=\"struct hacspec_halo2::PublicParams\">PublicParams</a>",1,["hacspec_halo2::PublicParams"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
