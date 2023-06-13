7c1414f8395893d91eb2fd6d56995f115e73496b
Rasmus T. Bjerg <rs.bjeg@gmail.com>
2023-06-13 17:32:13 +0200

    include pasta, maybe final now

--- implementors/core/ops/try_trait/trait.Try.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {
-"abstract_integers":[],
-"hacspec_lib":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/num_traits/ops/euclid/trait.CheckedEuclid.js
+++
@@ -1,6 +0,0 @@
-(function() {var implementors = {
-"abstract_integers":[],
-"hacspec_lib":[],
-"num":[],
-"num_traits":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
